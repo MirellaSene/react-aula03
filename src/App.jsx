661acf24be13d29ec426a69596d22bb4710d8ab3
MIRELLA DA SILVA SENE <25162437@edu119.local>
2026-02-27 10:56:39 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Heading from './component/Heading.jsx'
+
+export function App() {
+  return (
+    <div>
+      <h1>OIIIII🩷</h1>
+      <Heading />
+    </div>
+  )
+}
---
+++ src/component/Heading.jsx
@@ -0,0 +1,13 @@
+import styles from './Heading.module.css'
+
+function Heading() {
+  const classes = `${styles.heading} ${styles.cyan}`
+  
+  return (
+    <h1 className={classes}>
+      Olá Mundo 🌍
+    </h1>
+  )
+}
+
+export default Heading
